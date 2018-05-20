2c312c1a3fb94f93d5281fc248a74983cfac9b65
arshidnippon <34038733+arshidnippon@users.noreply.github.com>
2018-05-20 23:39:39 +0800

    Add files via upload

---
+++ EMEDEJB/src/java/emed/EMEDSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emed;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author dont know
+ */
+@Stateless
+public class EMEDSessionBean implements EMEDSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
